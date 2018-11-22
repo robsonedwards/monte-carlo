1922a242326450970a4cd3d53a1dbefd37663605
Robson Edwards <robson.edwards@gmail.com>
2018-11-22 10:17:16 +0000

    Apply changes from this morning

--- simulate.R
+++ simulate.R
@@ -1,9 +1,12 @@
+# 
+
 require("dslabs") 
 require("dplyr")
 require("ggplot2")
-#TODO: Do I also need to import magrittr #require("magrittr")
+# TODO: Do I also need to import magrittr #require("magrittr")
 
+
+# Data Wrangling. 
 data("polls_us_election_2016")
 polls <- polls_us_election_2016
 results <- results_us_election_2016
- 
