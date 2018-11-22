938f0a554e94de23283ea4121f7d0f8fd7b9845f
Robson Edwards <robson.edwards@gmail.com>
2018-11-22 08:38:31 +0000

    Apply changes from yesterday

--- simulate.R
+++ simulate.R
@@ -1,3 +1,9 @@
-install.packages("dslabs")
-library("dslabs")
-help(package = "dslabs")
+require("dslabs") 
+require("dplyr")
+require("ggplot2")
+#TODO: Do I also need to import magrittr #require("magrittr")
+
+data("polls_us_election_2016")
+polls <- polls_us_election_2016
+results <- results_us_election_2016
+ 
